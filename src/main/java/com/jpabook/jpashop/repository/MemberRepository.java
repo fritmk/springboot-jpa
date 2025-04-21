23a8f7f8b86c257efbb0c1f0fafe100101cecef4
fritmk <hhaanbb@gmail.com>
2025-04-21 17:51:55 +0900

    feat: member service 개발

---
+++ src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,48 @@
+package com.jpabook.jpashop.repository;
+
+
+import com.jpabook.jpashop.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// 스프링 빈으로 등록
+// 컴포넌트 스캐의 대상이 돼서 스프링부트애플리케이션 어노테이션이 있으면 이 패키지와 패키지 하위의 있는 것들을 모두 스프링에 컴포넌트 스캔을 함.
+// 컴포넌트 스캔을 통해서 스프링 빈에 자동 등롣된다.
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+
+    // JAP가 제공하는 표준 어노테이션
+//    @PersistenceContext // jpa 엔티티 매니저를 스프링이 생성한 엔티티 매니저에 주입해준다.
+//    private EntityManager em; // 스프링이 entity manager 를 만들어서 주입해줌
+
+    private final EntityManager em; // spring data jpa 가 이렇게 사용하는 것을 지원해줌
+
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        // jpql - 엔티티 객체를 대상으로 조회를 한다고 봐야 함
+        return em.createQuery("select m from Member m", Member.class)
+              .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+
+}
---
+++ src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,56 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+//@Transactional // jpa의 모든 데이터 변경이나 어떤 로직들은 가급적이면 트랜잭션 안에서 실행되어야함
+@Transactional(readOnly = true) // 조회하는 곳에서 성능이 더 좋아짐
+//@AllArgsConstructor // 필드를 가지고 생성자를 만들어줌
+@RequiredArgsConstructor // final 인 필드만 가지고 생성자를 만들어줌
+public class MemberService {
+
+    // 기본적으로 인젝션 하면서 생성자에서 세팅하고 끝날 것들은 파이널로 잡음.
+    // 이외 필요한 필드가 생길 수 있으니 RequiredArgsConstructor 쓰는 것을 권장
+    private final MemberRepository memberRepository;
+
+    // 회원가입
+    @Transactional // 따로 설정한 건 이게 우선순위가 높음.
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    // 이렇게 하더라도 서버를 여러개 띄워놓고 동시에 추가되는 요청이 들어오면 중복될 수 있으므로
+    // 멀티스레드와 같은 상황을 고려하여, 데이터베이스 자체에 이름에 대한 유니터 제약을 걸어두는 것이 더 좋음.
+    private void validateDuplicateMember(Member member) {
+        // Exception
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+
+    // 회원 전체 조회
+    public List<Member> findMemers() {
+        return memberRepository.findAll();
+    }
+
+    // 한건
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+}
