0311492c48e93dfcc7f27d9cae518f79cf21b90c
fritmk <hhaanbb@gmail.com>
2025-04-27 11:53:38 +0900

    feat: 데이터 수정 - 변경 감지와 병합

--- src/main/java/com/jpabook/jpashop/service/ItemService.java
+++ src/main/java/com/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.jpabook.jpashop.service;
 
+import com.jpabook.jpashop.domain.item.Book;
 import com.jpabook.jpashop.domain.item.Item;
 import com.jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,22 @@ public class ItemService {
     @Transactional(readOnly = false)
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+
+    // 데이터 수정 - (1) 변경 감지
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+
+        // 사실 이렇게 하지 않아야한다.
+        // 나중에 이런 식으로 하면 유지보수할 때 어디서 수정해야하는 지 다 찾아야 함.
+        // 따라서 엔티티 안에 업데이트하는 메서드를 만들어 놓는 것이 좋음.
+
+        // findItem.change(name, price, stockQuantity); <- 이런 식으로
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
     }
 
 
