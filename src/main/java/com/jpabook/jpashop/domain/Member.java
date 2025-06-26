932f0e96f49cbb0a633da3cce61e5346e6958a92
fritmk <hhaanbb@gmail.com>
2025-06-26 23:16:55 +0900

    feat: API 개발 기본

--- src/main/java/com/jpabook/jpashop/domain/Member.java
+++ src/main/java/com/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package com.jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,8 +23,10 @@ public class Member {
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member") // 읽기 전용.
     private List<Order> orders = new ArrayList<>();
+
 
 
 
